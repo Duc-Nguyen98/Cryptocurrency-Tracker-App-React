b1f6101547216addb91a002e0ba36f4905752015
Nguyễn Quang Đức <ducnin1998@gmail.com>
2021-10-24 07:29:44 +0700

    setup project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/*==========================================
+Title:  Bee-Cryptocurrency-Tracker
+Date:   24-Oct-2021
+==========================================*/
+import React from "react";
+import styled from "./App.module.css";
+
+const App = () => {
+  return <div>App</div>;
+};
+export default App;
